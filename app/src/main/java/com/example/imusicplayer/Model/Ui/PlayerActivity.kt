f8a24f70ea39df6a8dc02e3e01013d21789eee11
dolonk9 <dolonk9@gmail.com>
2023-01-23 23:06:12 +0600

    All Activity Design Completed

---
+++ app/src/main/java/com/example/imusicplayer/Model/Ui/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.imusicplayer.Model.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.imusicplayer.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
