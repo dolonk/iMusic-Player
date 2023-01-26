e93edf645ccc2b525378278f6f133914368b9427
dolonk9 <dolonk9@gmail.com>
2023-01-27 00:24:54 +0600

    Add start song and player layout

--- app/src/main/java/com/example/imusicplayer/Model/Adapter/MusicAdapter.kt
+++ app/src/main/java/com/example/imusicplayer/Model/Adapter/MusicAdapter.kt
@@ -41,6 +41,8 @@ class MusicAdapter(private val context: Context, private val musicList: ArrayLis
 
         holder.root.setOnClickListener {
             val intent = Intent(context, PlayerActivity::class.java)
+            intent.putExtra("index",position)
+            intent.putExtra("class","MusicAdapter")
             ContextCompat.startActivity(context,intent,null)
         }
     }
--- app/src/main/java/com/example/imusicplayer/Model/Ui/PlayerActivity.kt
+++ app/src/main/java/com/example/imusicplayer/Model/Ui/PlayerActivity.kt
@@ -1,13 +1,55 @@
 package com.example.imusicplayer.Model.Ui
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.imusicplayer.R
+import com.example.imusicplayer.Service.Domain.DomainMusic
+import com.example.imusicplayer.databinding.ActivityMainBinding
+import com.example.imusicplayer.databinding.ActivityPlayListBinding
+import com.example.imusicplayer.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPlayerBinding
+    companion object {
+        lateinit var musicListPa: ArrayList<DomainMusic>
+        var songPosition: Int = 0
+        var mediaPlayer: MediaPlayer? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
         setTheme(R.style.coolPinkNav)
-        setContentView(R.layout.activity_player)
+        setContentView(binding.root)
+
+        startSong()
+        initializeLayout()
+    }
+
+    private fun initializeLayout() {
+        Glide.with(this).load(musicListPa[songPosition].imageUri)
+            .apply(RequestOptions().placeholder(R.drawable.music_icon))
+            .into(binding.pSongPicID)
+        binding.pSongTittleID.text = musicListPa[songPosition].title
+    }
+
+
+    private fun startSong() {
+        songPosition = intent.getIntExtra("index", 0)
+        when (intent.getStringExtra("class")) {
+            "MusicAdapter" -> {
+                musicListPa = ArrayList()
+                musicListPa.addAll(MainActivity.MusicListMA)
+
+                if (mediaPlayer == null) mediaPlayer = MediaPlayer()
+                mediaPlayer!!.reset()
+                mediaPlayer!!.setDataSource(musicListPa[songPosition].path)
+                mediaPlayer!!.prepare()
+                mediaPlayer!!.start()
+            }
+        }
     }
 }
